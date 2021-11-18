ec6768a62d3b39c0408bf2b261b4e540b859e750
phuclm <phuclm@mirabo.com.vn>
2021-11-18 10:16:23 +0700

    test publish

--- packages/arena/example/app-entrypoint.ts
+++
@@ -1,7 +0,0 @@
-// import method to setup local/development environment
-import { listen } from "../src";
-
-// import arena configuration file
-import arena from "./arena.config";
-
-listen(arena);
--- packages/transport/uwebsockets-transport/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { uWebSocketClient } from "./uWebSocketClient";
-export { uWebSocketsTransport, TransportOptions } from "./uWebSocketsTransport"
